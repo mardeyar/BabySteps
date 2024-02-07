f2592b3bd34f7c0a91f95265a0ab3fcbda067a6e
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-07 16:48:01 -0400

    Reworked packages and started fresh

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#01d68b',
     alignItems: 'center',
     justifyContent: 'center',
   },
