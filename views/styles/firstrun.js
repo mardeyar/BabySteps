ff2f85cd9dc0874b31a337877765172d9e9533e4
Mark Deyarmond <deyarmondmark@gmail.com>
2024-02-26 17:09:13 -0400

    Styling added for initial setup

---
+++ views/styles/firstrun.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const infoStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    input: {
+        height: 40,
+        width: '100%',
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginBottom: 8,
+    },
+    photo: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        marginBottom: 10,
+    },
+});
