a69bd985a43ad4466f1ff956a10e26d3c8af6220
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-01 16:55:09 -0400

    Removed disclaimerCheck, using dataExists now

--- database/DatabaseManager.js
+++ database/DatabaseManager.js
@@ -6,7 +6,7 @@ const db = SQLite.openDatabase('babysteps.db');
 const initialSetup = () => {
     db.transaction(tx => {
         tx.executeSql(
-            'CREATE TABLE IF NOT EXISTS baby_profile (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT NOT NULL, middle_name TEXT, last_name TEXT NOT NULL, gender TEXT NOT NULL, dob TEXT NOT NULL, photo TEXT, disclaimer BOOLEAN NOT NULL DEFAULT 0)',
+            'CREATE TABLE IF NOT EXISTS baby_profile (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT NOT NULL, middle_name TEXT, last_name TEXT NOT NULL, gender TEXT NOT NULL, dob DATE NOT NULL, photo TEXT, disclaimer BOOLEAN NOT NULL DEFAULT 0)',
             [],
             (_, result) => {
                 console.log('Table named "baby_profile" created successfully!');
@@ -33,35 +33,20 @@ const createProfile = (firstName, middleName, lastName, gender, dob, photo) => {
     });
 };
 
-const disclaimerCheck = (acknowledged) => {
+// This function exists to check table for data; if there is data, skip first run pages on app launch
+const checkData = (callback) => {
     db.transaction(tx => {
         tx.executeSql(
-            'UPDATE baby_profile SET disclaimer = ? WHERE id = 1',
-            [acknowledged ? 1 : 0],
+            'SELECT * FROM baby_profile',
+            [],
             (_, result) => {
-                console.log('User acknowledged disclaimer');
+                callback(result.rows._array);
             },
             (_, error) => {
-                console.log('Acknowledgment error: ', error);
-            },
+                console.log('Error fetching data from table "baby_profile": ', error);
+            }
         );
     });
 };
 
-// This function exists to check table for data; if there is data, skip first run pages on app launch
-// const checkData = (callback) => {
-//     db.transaction(tx => {
-//         tx.executeSql(
-//             'SELECT * FROM baby_profile',
-//             [],
-//             (_, result) => {
-//                 callback(result.rows._array);
-//             },
-//             (_, error) => {
-//                 console.log('Error fetching data from table "baby_profile": ', error);
-//             }
-//         );
-//     });
-// };
-
-export { initialSetup, createProfile, disclaimerCheck };
+export { initialSetup, createProfile, checkData };
