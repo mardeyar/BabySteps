6ecd418f507d99830868103b9be2f3b0f71f216d
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-06 15:38:07 -0400

    Moved file to different location

---
+++ views/Pages/Disclaimer.js
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+const Disclaimer = () => {
+
+    return (
+        <View>
+            <Text>About</Text>
+            <Text>BabySteps is designed to help you keep track of each and every milestone your baby does through as they grow up. From their first arrival home from the hospital 
+            to their first steps, you can keep every memory of your little ones amazing accomplishments close by. As they age each week, you can attach photos or videos to each 
+            milestone for a keepsake and look back on those memories as the grow up. Think of BabySteps as your digital babybook.</Text>
+            <Text>Disclaimer</Text>
+            <Text>BabySteps does not communicate with any other services outside of your device. This means that any and all data about your baby will be kept on your device 
+            and not seen by any 3rd parties, keeping your information private to only you. Due to this storage method, your data being contained to your device means there are 
+            no backup options should you delete the app; deleting BabySteps from your device means deleting all data associated with BabySteps that you have entered up to this 
+            point.</Text>
+        </View>
+    );
+};
+
+export default Disclaimer;
