22e255543c43f5fb402a7ea3dc1c6f6bb5154876
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-06 15:35:04 -0400

    Bottom nav configured

--- App.js
+++ App.js
@@ -1,47 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { initialSetup, checkData } from './database/DatabaseManager';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { profileSetup, disclaimerCheckSetup, checkData, milestoneSetup } from './database/DatabaseManager';
 
-import Information from './views/FirstRun/Information';
-// import FirstRunInput from './views/FirstRun/Information'
-// import Disclaimer from './views/FirstRun/Disclaimer';
-import MainPage from './views/Main/MainPage';
+import CreateProfile from './views/Pages/CreateProfile';
+import MainPage from './views/Pages/MainPage';
+import SettingsPage from './views/Pages/Settings';
 
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  // Initialize these boolean methods to false at start, changing to true once the conditions are met
-  const [babyProfileExists, setBabyProfileExists] = useState(false);
-  const [initialSetupComplete, setInitialSetupComplete] = useState(false);
 
+  // Need to run the setup methods from DB manager to create tables if they don't exist
   useEffect(() => {
-    initialSetup();
-    setInitialSetupComplete(true);
+    profileSetup();
+    milestoneSetup();
   }, []);
 
-  useEffect(() => {
-    if (initialSetupComplete) {
-      checkData((result) => {
-        if (result.length > 0) {
-          // Baby profile exists, no need to run initial setup
-          setBabyProfileExists(true);
-        }
-      });
-    }
-  }, [initialSetupComplete]);
-
   return (
-    <>
-      {babyProfileExists ? (
-        // Render the main app page if baby profile exists
-        <MainPage />
-      ) : (
-        // Render initial setup if not
-        <>
-          <Information />
-          {/* <Disclaimer /> */}
-        </>
-      )}
-    </>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="Home" 
+          component={MainPage} 
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='home' color={color} size={size} />
+            ),
+          }}
+          />
+        <Tab.Screen name="Milestone" component={CreateProfile} />
+        <Tab.Screen 
+          name="Settings Page" 
+          component={SettingsPage} 
+          options={{
+            headerShown: false,
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name='settings' color={color} size={size} />
+            ),
+            }}/>
+      </Tab.Navigator>
+      <StatusBar style='auto' />
+    </NavigationContainer>
   );
 }
