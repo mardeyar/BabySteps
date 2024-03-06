d5e894698ed4dca0a2058dc3860d962c1d2eb4f2
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-06 15:37:07 -0400

    Added images component

---
+++ assets/images/index.js
@@ -0,0 +1,10 @@
+const IMAGES = {
+    ADAPTIVEICON: require('./adaptive-icon.png'),
+    FAVICON: require('./favicon.png'),
+    ICON: require('./icon.png'),
+    SPLASH: require('./splash.png'),
+    HOME: require('./home.png'),
+    SETTINGS: require('./settings.png')
+}
+
+export default IMAGES;
