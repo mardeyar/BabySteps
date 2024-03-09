e06f421bafd76b34b1449a491a2bf848c7d6d39d
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-09 11:47:55 -0400

    Created profile page

--- views/Pages/Profile.js
+++ views/Pages/Profile.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Card } from '@rneui/themed';
+import { View, Text, Image } from 'react-native';
+import { checkData } from '../../database/DatabaseManager';
+import { useFocusEffect } from '@react-navigation/native';
 
 const Profile = () => {
-    return (
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [dob, setDob] = useState('');
+    const [babyPhoto, setBabyPhoto] = useState('');
+
+    useFocusEffect(() => {
+        checkData((data) => {
+            if (data.length > 0) {
+                setFirstName(data[0].first_name);
+                setLastName(data[0].last_name);
+                setDob(data[0].dob);
+                setBabyPhoto(data[0].photo);
+            }
+        });
+    });
+
+    return(
         <View>
-            <Text>Profile Screen</Text>
+            <Image source={{ uri: babyPhoto}} style={{ width: 300, height: 300}} />
+            <Text>{firstName} {lastName}</Text>
+            <Text>{dob}</Text>    
         </View>
-    );
+    )
 }
 
 export default Profile;
