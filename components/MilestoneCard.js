306b1820f4c1b62a3893886d88268cdafcb360c0
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-11 13:58:01 -0300

    Started milestone card display component

---
+++ components/MilestoneCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View } from "react-native";
+import { Card } from "@rneui/themed";
+
+const MilestoneCard = () => {
+    return (
+        <View>
+            <Card.Title>Milestone</Card.Title>
+            <Card.Divider />
+            <View>
+                
+            </View>
+        </View>
+        
+    );
+}
+
+export default MilestoneCard;
