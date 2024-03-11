dfb0ef2a1afb2bd0cb0ab1a484fbf265f185ff34
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-11 13:57:19 -0300

    Pass value and setValue as props

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -3,9 +3,9 @@ import { View } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 
-const GenderDropdown = () => {
+const GenderDropdown = ({ value, setValue }) => {
     const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
+    //const [value, setValue] = useState(null); <--- comment out this lin ehere, pass as a prop
     const [items, setItems] = useState([
         { label: 'Boy', value: 'Boy'},
         { label: 'Girl', value: 'Girl'}
