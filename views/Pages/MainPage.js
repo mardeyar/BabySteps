62ba0ec325fe2079105ed7a124967dcd8c9784b9
Mark Deyarmond <deyarmondmark@gmail.com>
2024-03-18 17:23:20 -0300

    MainPage now displays memories

--- views/Pages/MainPage.js
+++ views/Pages/MainPage.js
@@ -1,27 +1,101 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image } from 'react-native';
-import { checkData } from '../../database/DatabaseManager';
+import { View, Text, Image, ScrollView, TouchableOpacity, Modal, StyleSheet, TouchableWithoutFeedback, Alert } from 'react-native';
+import { checkData, fetchMilestones, deleteMemory } from '../../database/DatabaseManager';
 import { useFocusEffect } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+
+import MilestoneCard from '../../components/MilestoneCard';
 
 const Home = () => {
-    const [firstName, setFirstName] = useState('');
-    const [babyPhoto, setBabyPhoto] = useState('');
+    const [milestones, setMilestones] = useState([]);
+    const [selectedPhoto, setSelectedPhoto] = useState(null);
+    const [selectedMemory, setSelectedMemory] = useState(null);
+    const navigation = useNavigation();
 
-    useFocusEffect(() => {
-        checkData((data) => {
-            if (data.length > 0) {
-                setFirstName(data[0].first_name);
-                setBabyPhoto(data[0].photo);
-            }
-        });
-    });
+    // This block will refresh the page everytime it opens up memories pop up instantly
+    useFocusEffect(
+        React.useCallback(() => {
+            fetchMilestones((data) => {
+                const sorted = data.sort((a, b) => new Date(b.milestone_date) - new Date(a.milestone_date));
+                setMilestones(sorted);
+            });
+        }, [])
+    );
 
-    return(
-        <View>
-            {firstName && <Text>Hello {firstName}!</Text>}
-            {babyPhoto && <Image source={{ uri: babyPhoto }} style={{ width: 100, height: 100}} />}
-        </View>
-    )
-}
+    const handlePhotoTap = (photo) => {
+        setSelectedPhoto(photo);
+    }
+
+    const closePhoto = () => {
+        setSelectedPhoto(null);
+    }
+
+    const handleDeleteMemory = (milestoneId) => {
+        setSelectedMemory(milestoneId);
+        Alert.alert(
+            'Delete Memory', 
+            'Are you sure you want to delete this memory?',
+            [
+                {
+                    text: 'Cancel',
+                    style: 'cancel'
+                },
+                { 
+                    text: 'Delete', 
+                    onPress: () => {
+                        deleteMemory(milestoneId, () => {
+                            setMilestones(prevMilestones => prevMilestones.filter(milestone => milestone.milestone_id !== milestoneId));
+                        });
+                    }
+                },
+            ],
+            { cancelable: false }
+        );
+    };
+
+    return (
+        <ScrollView>
+            {milestones.length === 0 ? (
+                <View>
+                    <Text>You have no memories posted</Text>
+                </View>
+            ) : (
+                milestones.map((milestone, index) => (
+                    <MilestoneCard 
+                        key={index}
+                        milestoneDate={milestone.milestone_date}
+                        milestoneName={milestone.milestone_name}
+                        milestoneInfo={milestone.milestone_info}
+                        photo={milestone.photo}
+                        onPressPhoto={() => handlePhotoTap(milestone.photo)}
+                        onLongPressDelete={() => handleDeleteMemory(milestone.milestone_id)}
+                    />
+                ))
+            )}
+
+            {/* Fullscreen photo viewer */}
+            <Modal visible={!!selectedPhoto} transparent={true} onRequestClose={closePhoto}>
+                <TouchableWithoutFeedback onPress={closePhoto}>
+                    <View style={styles.modalBackground}>
+                        <Image source={{ uri: selectedPhoto }} resizeMode="contain" style={styles.fullScreenImage} />
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    modalBackground: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.9)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    fullScreenImage: {
+        width: '100%',
+        height: '100%',
+    },
+});
 
 export default Home;
