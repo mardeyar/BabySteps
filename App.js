c59da52fc04d5fecc27187237d05e6fd8a4b505b
Mark Deyarmond <deyarmondmark@gmail.com>
2024-04-04 09:20:19 -0300

    Added app wide theming

--- App.js
+++ App.js
@@ -3,30 +3,43 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
-import { profileSetup, milestoneSetup } from './database/DatabaseManager';
+import { profileSetup, memorySetup } from './database/DatabaseManager';
 import { useFonts } from 'expo-font';
 
-import Milestone from './views/Pages/Milestone';
+import Memory from './views/Pages/Memory';
 import MainPage from './views/Pages/MainPage';
 import SettingsPage from './views/Pages/Settings';
+
+import { HeaderStyle, AppColor } from './views/styles/Global';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
 
   // Decalre the fonts the app will use
-  useFonts({
+  const [fontsLoaded] = useFonts({
     'Urbanist': require('./assets/fonts/Urbanist.ttf'),
+    'NotoSans': require('./assets/fonts/NotoSans.ttf'),
+    'LatoBold': require('./assets/fonts/LatoBold.ttf'),
+    'LatoLight': require('./assets/fonts/LatoLight.ttf'),
+    'LatoRegular': require('./assets/fonts/LatoRegular.ttf'),
+    'LatoThin': require('./assets/fonts/LatoThin.ttf'),
+    'Carrois': require('./assets/fonts/Carrois.ttf')
   });
 
   // Need to run the setup methods from DB manager to create tables if they don't exist
   useEffect(() => {
     profileSetup();
-    milestoneSetup();
-  });
+    memorySetup();
+  }, []);
+
+  // Wait briefly to allow font loading
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    <NavigationContainer>
+      <NavigationContainer theme={AppColor}>
       <Tab.Navigator>
         <Tab.Screen 
           name="Home" 
@@ -35,11 +48,15 @@ export default function App() {
             tabBarIcon: ({ color, size }) => (
               <Ionicons name='home' color={color} size={size} />
             ),
+            title: 'Babysteps',
+            headerTitleStyle: HeaderStyle.text,
+            headerTitleAlign: 'left',
+            headerTintColor: '#9FB7B9'
           }}
           />
         <Tab.Screen 
           name="Add Memory" 
-          component={Milestone} 
+          component={Memory} 
           options={{
             tabBarIcon: ({ color, size }) => (
               <Ionicons name='add-circle-outline' color={color} size={size} />
@@ -59,5 +76,6 @@ export default function App() {
       </Tab.Navigator>
       <StatusBar style='auto' />
     </NavigationContainer>
+    
   );
 }
