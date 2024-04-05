e218999b4caa6d6968d70d9f101eb0f7a2b0ae3c
Mark Deyarmond <deyarmondmark@gmail.com>
2024-04-05 16:13:28 -0300

    Created application disclaimers on state and data storage

--- views/Pages/Disclaimer.js
+++ views/Pages/Disclaimer.js
@@ -1,20 +1,29 @@
-import { Text, View, Button } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+import { AppInformation, InfoContainer } from "../styles/AppInfo";
 
 const Disclaimer = () => {
 
     return (
-        <View>
-            <Text>About</Text>
-            <Text>BabySteps is designed to help you keep track of each and every milestone your baby does through as they grow up. From their first arrival home from the hospital 
-            to their first steps, you can keep every memory of your little ones amazing accomplishments close by. As they age each week, you can attach photos or videos to each 
-            milestone for a keepsake and look back on those memories as the grow up. Think of BabySteps as your digital babybook.</Text>
-            <Text>Disclaimer</Text>
-            <Text>BabySteps does not communicate with any other services outside of your device. This means that any and all data about your baby will be kept on your device 
-            and not seen by any 3rd parties, keeping your information private to only you. Due to this storage method, your data being contained to your device means there are 
-            no backup options should you delete the app; deleting BabySteps from your device means deleting all data associated with BabySteps that you have entered up to this 
-            point.</Text>
-            <Button title="I accept" />
-        </View>
+        <ScrollView>
+            <View style={InfoContainer.body}>
+                <Text style={AppInformation.title}>Data Storage</Text>
+                <View style={AppInformation.divider} />
+                <Text style={AppInformation.bodyText}>BabySteps does not communicate with any other services outside of your device. This means that any and all data about your baby will be kept on your device 
+                and not seen by any 3rd parties, keeping your information private to only you.</Text>
+                <Text style={AppInformation.bodyText}>Due to this storage method, your data being contained to your device means there are 
+                no backup options should you delete the app; deleting BabySteps from your device means deleting all data associated with BabySteps that you have entered up to this 
+                point.</Text>
+
+                <View style={InfoContainer.spacer} />
+
+                <Text style={AppInformation.title}>App State</Text>
+                <View style={AppInformation.divider} />
+                <Text style={AppInformation.bodyText}>This app is currently in a <Text style={AppInformation.boldStyle}>beta release</Text> stage. 
+                This means that bugs are likely and certain features may not work as intended.</Text>
+            </View>
+
+        </ScrollView>
+        
     );
 };
 
