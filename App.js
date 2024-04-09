c1d3a8469faa2319384393067d8f0f2471baab49
Mark Deyarmond <deyarmondmark@gmail.com>
2024-04-09 16:22:21 -0300

    Remove font refs and change status bar color to dark text

--- App.js
+++ App.js
@@ -1,42 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { profileSetup, memorySetup } from './database/DatabaseManager';
-import { useFonts } from 'expo-font';
 
 import Memory from './views/Pages/Memory';
 import MainPage from './views/Pages/MainPage';
 import SettingsPage from './views/Pages/Settings';
+import CustomTitle from './components/HeaderTitle';
 
-import { HeaderStyle, AppColor } from './views/styles/Global';
+import { AppColor } from './views/styles/Global';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-
-  // Decalre the fonts the app will use
-  const [fontsLoaded] = useFonts({
-    'Urbanist': require('./assets/fonts/Urbanist.ttf'),
-    'NotoSans': require('./assets/fonts/NotoSans.ttf'),
-    'LatoBold': require('./assets/fonts/LatoBold.ttf'),
-    'LatoLight': require('./assets/fonts/LatoLight.ttf'),
-    'LatoRegular': require('./assets/fonts/LatoRegular.ttf'),
-    'LatoThin': require('./assets/fonts/LatoThin.ttf'),
-    'Carrois': require('./assets/fonts/Carrois.ttf')
-  });
 
   // Need to run the setup methods from DB manager to create tables if they don't exist
   useEffect(() => {
     profileSetup();
     memorySetup();
   }, []);
-
-  // Wait briefly to allow font loading
-  if (!fontsLoaded) {
-    return null;
-  }
 
   return (
       <NavigationContainer theme={AppColor}>
@@ -52,9 +36,8 @@ export default function App() {
               <Ionicons name='home' color={color} size={size} />
             ),
             title: 'Babysteps',
-            headerTitleStyle: HeaderStyle.text,
+            headerTitle: () => <CustomTitle title='Babysteps' />,
             headerTitleAlign: 'left',
-            headerTintColor: '#9FB7B9'
           }}
           />
         <Tab.Screen 
@@ -83,8 +66,7 @@ export default function App() {
             ),
             }}/>
       </Tab.Navigator>
-      <StatusBar style='auto' />
+      <StatusBar style='dark' />
     </NavigationContainer>
-    
   );
 }
