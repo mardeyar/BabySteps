d9eef3307f2502d67c8bf93dbcfa69508504fa4c
Mark Deyarmond <deyarmondmark@gmail.com>
2024-04-15 15:36:34 -0300

    Small changes

--- components/HeaderTitle.js
+++ components/HeaderTitle.js
@@ -4,8 +4,8 @@ import { Text } from 'react-native';
 const CustomTitle = ({ title }) => {
     return (
         <Text style={{ margin: 0, padding: 0 }}>
-          <Text style={{ fontSize: 38, fontWeight: '800', color: '#91A8AA' }}>{title.charAt(0)}</Text>
-          <Text style={{ fontSize: 22, fontWeight: '700', color: '#91A8AA' }}>{title.slice(1).toUpperCase()}</Text>
+          <Text style={{ fontSize: 42, fontWeight: '800', color: '#91A8AA' }}>{title.charAt(0)}</Text>
+          <Text style={{ fontSize: 26, fontWeight: '700', color: '#91A8AA' }}>{title.slice(1).toUpperCase()}</Text>
         </Text>
       );
 };
